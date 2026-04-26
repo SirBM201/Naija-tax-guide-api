8268d6d6620e2d2055904524dcd58cbe87569c17
SirBM201 <bms.concept@hotmail.com>
2026-04-26 15:27:51 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ import os
 from flask import Flask, session, request, g
 from flask_cors import CORS
 from dotenv import load_dotenv
-from flask.sessions import SecureCookieSessionInterface
 
 # Load environment variables
 load_dotenv()
